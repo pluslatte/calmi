bf71f65a5b6c15f593fbaf46c2b35aa0fdfed924
pluslatte <debrisfield0201@gmail.com>
2025-04-13 21:22:53 +0900

    状態ずれエラーを修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,23 @@
 import '@mantine/core/styles.css';
 import { ColorSchemeScript } from '@mantine/core';
 import ThemeProvider from "@/theme-provider";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <html lang="en">
+        <body>
+          <p>not mounted</p>
+        </body>
+      </html>
+    )
+  }
   return (
     <html lang="en">
       <head>
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -3,9 +3,19 @@
 
 import { ActionIcon, useMantineColorScheme } from '@mantine/core';
 import { IconSun, IconMoon } from '@tabler/icons-react';
+import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
     const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return (<p>not mounted</p>)
+    }
 
     return (
         <ActionIcon
