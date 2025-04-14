25e90d4b4d7f0ee36512bbbfa8e0595c78aa8bb7
pluslatte <debrisfield0201@gmail.com>
2025-04-14 21:23:50 +0900

    レイアウトをいい感じに

---
+++ src/components/AutoRefreshTimestamp.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { Text } from "@mantine/core";
+import { formatDistanceStrict, parseISO } from "date-fns";
+import { useEffect, useState } from "react";
+
+export default function AutoRefreshTimestamp({ iso }: { iso: string }) {
+    const [text, setText] = useState('');
+
+    const update = () => {
+        setText(formatDistanceStrict(new Date(), parseISO(iso), { addSuffix: true }));
+    }
+
+    useEffect(() => {
+        update();
+        const interval = setInterval(update, 3000);
+        return (() => {
+            clearInterval(interval);
+        });
+    }, []);
+
+    return (
+        <Text span c="dimmed">
+            {text}
+        </Text>
+    )
+}
--- src/components/MisskeyNote.tsx
+++ src/components/MisskeyNote.tsx
@@ -1,37 +1,41 @@
 import { useMisskeyApiClient } from "@/app/MisskeyApiClientContext";
-import { Avatar, Container, Grid, Text } from "@mantine/core";
+import { Avatar, Box, Container, Flex, Grid, Group, Text } from "@mantine/core";
 import { Note } from "misskey-js/entities.js";
+import AutoRefreshTimestamp from "./AutoRefreshTimestamp";
 
 export default function MisskeyNote({ note }: { note: Note }) {
     const misskeyApiClient = useMisskeyApiClient();
 
     return (
         <Container>
-            <Grid>
-                <Grid.Col span="content">
-                    <Avatar src={note.user.avatarUrl} radius={'md'} m="xs" />
-                </Grid.Col>
-                <Grid.Col span="auto" mt="4">
-                    <div>
-                        <Text
-                            fw="bold"
-                            span
-                        >
-                            {note.user.username}
-                        </Text>
-                        <Text
-                            fw="normal"
-                            c="dimmed"
-                            span
-                        >
-                            {note.user.host ? "@" + note.user.host : ''}
-                        </Text>
-                    </div>
-                    <Text>
-                        {note.text}
-                    </Text>
-                </Grid.Col>
-            </Grid>
+            <Flex align="start" gap="sm" wrap="nowrap">
+                <Avatar src={note.user.avatarUrl} radius={'md'} m="xs" />
+                <Box miw={0} mt="4" flex={1}>
+                    <Flex justify="space-between" align="center" wrap="nowrap" miw={0}>
+                        <Flex gap="xs" wrap="nowrap" align="center" miw={0} flex={1}>
+                            <Text
+                                fw="bold"
+                                truncate="end"
+                                maw="100%"
+                            >
+                                {note.user.username}
+                            </Text>
+                            <Text
+                                fw="normal"
+                                c="dimmed"
+                                truncate="end"
+                                maw="100%"
+                            >
+                                {note.user.host ? "@" + note.user.host : ''}
+                            </Text>
+                        </Flex>
+                        <div style={{ flexShrink: 0 }}>
+                            <AutoRefreshTimestamp iso={note.createdAt} />
+                        </div>
+                    </Flex>
+                    <Text>{note.text}</Text>
+                </Box>
+            </Flex>
         </Container>
     );
 }
