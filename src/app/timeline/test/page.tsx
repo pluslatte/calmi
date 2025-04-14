d9e8e07b7538b102a99e31df6d03ff0873f50cb1
pluslatte <debrisfield0201@gmail.com>
2025-04-14 19:41:22 +0900

    MisskeyNote を作ろう

---
+++ src/app/timeline/test/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import MisskeyNote from "@/components/MisskeyNote";
+import { useMisskeyApiClient } from "../../MisskeyApiClientContext";
+import { useEffect, useState } from "react";
+import { Note } from "misskey-js/entities.js";
+
+export default function Test() {
+    const misskeyApiClient = useMisskeyApiClient();
+
+    const [note, setNote] = useState<Note>();
+
+    useEffect(() => {
+        misskeyApiClient.request('notes/show', {
+            noteId: 'a6j845r6eg'
+        }).then((note) => { setNote(note) });
+    }, [])
+
+    return (
+        note ? <MisskeyNote note={note} /> : <p>Loading...</p>
+    )
+}
--- src/components/MisskeyNote.tsx
+++ src/components/MisskeyNote.tsx
@@ -1,9 +1,10 @@
 import { useMisskeyApiClient } from "@/app/MisskeyApiClientContext";
+import { Note } from "misskey-js/entities.js";
 
-export default function MisskeyNote() {
+export default function MisskeyNote({ note }: { note: Note }) {
     const misskeyApiClient = useMisskeyApiClient();
 
     return (
-        <></>
+        <p>{note.text}</p>
     );
 }
