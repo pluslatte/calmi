49407a7cabf508875eb178617cbe38226369ad9b
pluslatte <debrisfield0201@gmail.com>
2025-04-16 08:18:35 +0900

    Revert "refactor: タイムライン機能の責任分離と再構成"
    This reverts commit 2ce916c58eedca6f6083df2304be4fe3b07cb47a.

--- src/components/MisskeyTimelineContainer.tsx
+++ src/components/MisskeyTimelineContainer.tsx
@@ -2,7 +2,6 @@ import { Box, Flex, ScrollArea, Tabs, Tooltip } from "@mantine/core";
 import React, { memo, useRef, useState } from "react";
 import MisskeyTimeline, { TimelineType } from "@/components/MisskeyTimeline";
 import { IconGalaxy, IconHome, IconHomePlus, IconServer } from "@tabler/icons-react";
-import { TimelineHeader } from "./timeline/TimelineHeader";
 
 
 const MisskeyTimelineContainer = memo(function MisskeyTimelineContainer({
@@ -15,17 +14,29 @@ const MisskeyTimelineContainer = memo(function MisskeyTimelineContainer({
 
     return (
         <Flex direction="column" h="100%">
-            <TimelineHeader
-                timelineType={timelineType}
-                onChangeType={(type) => setTimelineType(type)}
-            />
+            <Tabs
+                value={timelineType}
+                variant="default"
+                onChange={(value) => setTimelineType(value as TimelineType)}
+            >
+                <Tabs.List justify="center" px="md" pt="xs">
+                    <Tooltip label="ホーム">
+                        <Tabs.Tab value="home" leftSection={<IconHome size={18} />}></Tabs.Tab>
+                    </Tooltip>
+                    <Tooltip label="ソーシャル">
+                        <Tabs.Tab value="social" leftSection={<IconHomePlus size={18} />}></Tabs.Tab>
+                    </Tooltip>
+                    <Tooltip label="ローカル">
+                        <Tabs.Tab value="local" leftSection={<IconServer size={18} />}></Tabs.Tab>
+                    </Tooltip>
+                    <Tooltip label="グローバル">
+                        <Tabs.Tab value="global" leftSection={<IconGalaxy size={18} />}></Tabs.Tab>
+                    </Tooltip>
+                </Tabs.List>
+            </Tabs>
             <ScrollArea viewportRef={scrollAreaRef} flex={1} type="auto">
                 <Box mr="sm">
-                    <MisskeyTimeline
-                        timelineType={timelineType}
-                        scrollAreaRef={scrollAreaRef}
-                        containerRef={containerRef}
-                    />
+                    <MisskeyTimeline timelineType={timelineType} scrollAreaRef={scrollAreaRef} containerRef={containerRef} />
                 </Box>
             </ScrollArea>
         </Flex>
