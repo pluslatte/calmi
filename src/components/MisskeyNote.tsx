b351eb3b92f2a4253b4ed2c995291da3af2b1d0d
pluslatte <debrisfield0201@gmail.com>
2025-04-17 18:00:02 +0900

    いったんこれでいいか

--- src/components/MisskeyNote.tsx
+++ src/components/MisskeyNote.tsx
@@ -26,10 +26,12 @@ const MisskeyNote = memo(function MisskeyNote({ note }: { note: Note }) {
                     {/* ユーザー情報（2段表示） */}
                     <Box>
                         {/* ユーザー名 */}
-                        <MfmObject
-                            mfmNodes={mfm.parse(note.user.name ? `**${note.user.name}**` : "")}
-                            assets={{ host: note.user.host, emojis: note.emojis }}
-                        />
+                        <Text size="md" lineClamp={1}>
+                            <MfmObject
+                                mfmNodes={mfm.parse(note.user.name ? `**${note.user.name}**` : "")}
+                                assets={{ host: note.user.host, emojis: note.emojis }}
+                            />
+                        </Text>
                         {/* ユーザーID */}
                         <Text size="xs" c="dimmed" lineClamp={1}>
                             @{note.user.username}
