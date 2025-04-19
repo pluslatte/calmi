7f5394afd8fee31f33f9a3edd6ce2d063448c1e7
pluslatte <debrisfield0201@gmail.com>
2025-04-19 18:03:17 +0900

    #26 その他、テーマカラー指定を修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="description" content="静かに Misskey を使いたい人のためのクライアント" />
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="theme-color" content="#007bff" />
+      <meta name="theme-color" content="#282828" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
       <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       <link rel="manifest" href="/manifest.json" />
