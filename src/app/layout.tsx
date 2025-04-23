51d1f14f0663f49a36d2014277abcc00afa6c24a
pluslatte <debrisfield0201@gmail.com>
2025-04-24 00:02:41 +0900

    アイコン修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   if (!mounted) {
     return (
       <html lang="en">
+        <head>
+          <title>calmi</title>
+          <link rel="icon" href="/icons/favicon.ico" type="image/x-icon" />
+          <link rel="shortcut icon" href="/icons/favicon.ico" type="image/x-icon" />
+        </head>
         <body>
           <p>not mounted</p>
         </body>
@@ -25,20 +30,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   }
   return (
     <html lang="en">
-      <title>calmi</title>
-      <meta name="application-name" content="calmi" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="calmi" />
-      <meta name="description" content="静かに Misskey を使いたい人のためのクライアント" />
-      <meta name="format-detection" content="telephone=no" />
-      <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="theme-color" content="#282828" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <head>
+        <title>calmi</title>
+        <meta name="application-name" content="calmi" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="calmi" />
+        <meta name="description" content="静かに Misskey を使いたい人のためのクライアント" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#282828" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-      <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="icon" href="/icons/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" type="image/x-icon" />
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body>
         <ThemeProvider>
           <MisskeyApiProvider>
