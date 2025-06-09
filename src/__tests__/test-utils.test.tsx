d926f57340de25922aa8a99389b5abc46d425539
pluslatte <debrisfield0201@gmail.com>
2025-06-09 19:15:18 +0900

    さっきのの基本的なテスト

--- src/__tests__/test-utils.test.tsx
+++ src/__tests__/test-utils.test.tsx
@@ -1,26 +1,61 @@
 import { describe, expect, test } from "vitest";
 import { renderWithProviders, screen } from "./utils/test-utils";
+import { useSession } from "next-auth/react";
+import { Button } from "@mantine/core";
 
 function TestComponent() {
     return <div>Hello Test World!</div>;
 }
 
-describe('test-utils', () => {
-    test('renderWithProviders is ok', () => {
+function SessionTestComponent() {
+    const { data: session, status } = useSession();
+
+    if (status === 'loading') return <div>Loading...</div>;
+    if (status === 'unauthenticated') return <div>Not authenticated</div>;
+
+    return <div>Hello {session?.user?.name}</div>;
+}
+
+function MantineTestComponent() {
+    return <Button>Test Button</Button>;
+}
+
+describe('test-utils renderProvider', () => {
+
+    test('simply render', () => {
         renderWithProviders(<TestComponent />);
         expect(screen.getByText('Hello Test World!')).toBeInTheDocument();
     });
 
-    test('Cover session', () => {
-        const mockSession = {
-            user: { name: 'Test User', email: 'test@example.com' },
-            expires: '2024-12-31'
-        };
+    describe('SessionProvider', () => {
 
-        renderWithProviders(<TestComponent />, {
-            session: mockSession
+        test('authenticated', () => {
+            const mockSession = {
+                user: { name: 'Test User', email: 'test@example.com' },
+                expires: '2024-12-31'
+            };
+
+            renderWithProviders(<SessionTestComponent />, {
+                session: mockSession
+            });
+
+            expect(screen.getByText('Hello Test User')).toBeInTheDocument();
         });
 
-        expect(screen.getByText('Hello Test World!')).toBeInTheDocument();
+        test('unauthenticated', () => {
+            renderWithProviders(<SessionTestComponent />, {
+                session: null
+            });
+
+            expect(screen.getByText('Not authenticated')).toBeInTheDocument();
+        });
     });
+
+    describe('MantineProvider', () => {
+        test('render a button', () => {
+            renderWithProviders(<MantineTestComponent />);
+
+            expect(screen.getByRole('button', { name: 'Test Button' })).toBeInTheDocument();
+        })
+    })
 });
