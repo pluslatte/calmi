6e45846538a20f8732ccbb6ab3088708a0c10606
pluslatte <debrisfield0201@gmail.com>
2025-06-12 17:36:49 +0900

    認証を絞る機能 ALLOWED_GITHUB_USERS

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
 import { handlers } from "@/../auth";
 
 export const { GET, POST } = handlers;
-export const runtime = "edge";
+// export const runtime = "edge"; // これログインできるユーザーを絞る機能を追加したときにうまく動かなかった
