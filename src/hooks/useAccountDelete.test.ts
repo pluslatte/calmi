cb52d0e6c8f12f09cc8fe2559ab46dc3f35dd41a
pluslatte <debrisfield0201@gmail.com>
2025-06-12 20:28:26 +0900

    list tests

---
+++ src/hooks/useAccountDelete.test.ts
@@ -0,0 +1,37 @@
+import { describe, it } from "vitest";
+
+describe('when deleteAccount is called', () => {
+    describe('success case', () => {
+        it('isDeleting is properly updated', () => {
+            // fill 
+        });
+
+        it('onAccountDeleted is called once on success', () => {
+            // fill 
+        })
+    })
+
+    describe('failure case', () => {
+        it('isDeleting is properly updated', () => {
+            // fill 
+        });
+
+        it('onAccountDeleted is NOT called', () => {
+            // fill 
+        });
+    });
+});
+
+describe('returns', () => {
+    it('isDeleting and deleteAccount', () => {
+        // fill
+    });
+
+    it('isDeleting is boolean', () => {
+        // fill
+    });
+
+    it('deleteAccount is () => void', () => {
+        // fill
+    });
+});
