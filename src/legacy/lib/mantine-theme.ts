720c98524546c38cff36e6928efe0dae5b41b293
pluslatte <debrisfield0201@gmail.com>
2025-06-13 20:35:42 +0900

    remove legacy files

--- src/legacy/lib/mantine-theme.ts
+++
@@ -1,7 +0,0 @@
-import { MantineThemeOverride } from '@mantine/core';
-
-export const theme: MantineThemeOverride = {
-    primaryColor: 'cyan',
-    defaultRadius: 'md',
-    fontFamily: 'sans-serif',
-};
