979eaa0c07bcb5f5184e65d18ab1bb7a109af286
pluslatte <debrisfield0201@gmail.com>
2025-10-29 12:20:15 +0900

    axum テスト

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", routing::get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
