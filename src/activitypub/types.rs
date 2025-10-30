8cf35600c9d59619944b2033fa2ec0f8a113c07c
pluslatte <debrisfield0201@gmail.com>
2025-10-31 01:31:30 +0900

    Add serde attributes to handle optional fields in activitypub types

--- src/activitypub/types.rs
+++ src/activitypub/types.rs
@@ -8,6 +8,7 @@ pub enum ObjectOrString {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
 pub enum ObjectBased {
     Object(ObjectExtended),
     Activity(ActivityExtended),
@@ -15,6 +16,7 @@ pub enum ObjectBased {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
 pub enum ObjectExtended {
     Object(Object),
     Person(Person),
@@ -22,12 +24,14 @@ pub enum ObjectExtended {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
 pub enum ActivityExtended {
     Activity(Activity),
     Create(Create),
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
 pub enum CollectionExtended {
     Collection(Collection),
     OrderedCollection(OrderedCollection),
@@ -36,84 +40,135 @@ pub enum CollectionExtended {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Object {
-    #[serde(rename = "@context")]
+    #[serde(rename = "@context", skip_serializing_if = "Option::is_none")]
     pub context: Option<Vec<String>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
-    #[serde(rename = "type")]
+
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
     pub r#type: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub to: Option<Vec<String>>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Person {
-    #[serde(rename = "@context")]
+    #[serde(rename = "@context", skip_serializing_if = "Option::is_none")]
     pub context: Option<Vec<String>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
-    #[serde(rename = "type")]
+
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
     pub r#type: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub inbox: Option<Box<ObjectOrString>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub outbox: Option<Box<ObjectOrString>>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Note {
-    #[serde(rename = "@context")]
+    #[serde(rename = "@context", skip_serializing_if = "Option::is_none")]
     pub context: Option<Vec<String>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
-    #[serde(rename = "type")]
+
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
     pub r#type: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub to: Option<Vec<String>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub content: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub attributed_to: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub published: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Activity {
-    #[serde(rename = "@context")]
+    #[serde(rename = "@context", skip_serializing_if = "Option::is_none")]
     pub context: Option<Vec<String>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
-    #[serde(rename = "type")]
+
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
     pub r#type: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub actor: Option<Box<ObjectOrString>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub object: Option<Box<ObjectExtended>>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Create {
-    #[serde(rename = "@context")]
+    #[serde(rename = "@context", skip_serializing_if = "Option::is_none")]
     pub context: Option<Vec<String>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
-    #[serde(rename = "type")]
+
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
     pub r#type: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub actor: Option<Box<ObjectOrString>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub object: Option<Box<ObjectOrString>>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Collection {
-    #[serde(rename = "@context")]
+    #[serde(rename = "@context", skip_serializing_if = "Option::is_none")]
     pub context: Option<Vec<String>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
-    #[serde(rename = "type")]
+
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
     pub r#type: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub total_items: Option<usize>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct OrderedCollection {
-    #[serde(rename = "@context")]
+    #[serde(rename = "@context", skip_serializing_if = "Option::is_none")]
     pub context: Option<Vec<String>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
-    #[serde(rename = "type")]
+
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
     pub r#type: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub total_items: Option<usize>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub ordered_items: Option<Vec<ObjectOrString>>,
 }
