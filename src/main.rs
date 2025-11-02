8ee21930d9eae31db3d6c7c1087395d2895e4f71
pluslatte <debrisfield0201@gmail.com>
2025-11-02 14:32:07 +0900

    データベースにつなげるかテスト

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ use calmi::app;
 use calmi::app_state;
 use calmi::config;
 use calmi::storage;
+use sea_orm::Database;
 
 #[tokio::main]
 async fn main() {
-    let database_url = std::env::var("DATABASE_URL").expect("Database URL must be set");
+    let database_url = std::env::var("DATABASE_URL").expect("env DATABASE_URL must be set");
+
+    let db = Database::connect(database_url)
+        .await
+        .expect("Failed to connect to database");
 
     let config = config::Config::default();
     let storage = storage::memory::MemoryStorage::new();
